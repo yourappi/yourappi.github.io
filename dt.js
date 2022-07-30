373a9b8dcc0a7a19e7823b7431b51d54df3a480a
yourappi <110261803+yourappi@users.noreply.github.com>
2022-07-30 16:52:28 -0500

    Create dt.js

---
+++ dt.js
@@ -0,0 +1,38 @@
+data = [
+    {
+        "place": "New York City",
+        "lat": "40.71455",
+        "lon": "-74.007124",
+        "deg": "⛅️️+40°C"
+    },
+    {
+        "place": "San Francisco",
+        "lat": "37.7771187",
+        "lon": "-122.4196396",
+        "deg": "128"
+    },
+    {
+        "place": "Santa Cruz",
+        "lat": "36.9740181",
+        "lon": "-122.0309525",
+        "deg": "990"
+    },
+    {
+        "place": "Santa Barbara",
+        "lat": "34.4193802",
+        "lon": "-119.6990509",
+        "deg": "40F"
+    },
+    {
+        "place": "Tucson",
+        "lat": "32.22155",
+        "lon": "-110.9697571",
+        "deg": "60"
+    },
+    {
+        "place": "Washington DC",
+        "lat": "38.8903694",
+        "lon": "-77.0319595",
+        "deg": "70"
+    }
+]
